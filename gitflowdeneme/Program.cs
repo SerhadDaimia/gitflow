567adfd63405a0e95b0e791f42e7ea05b2999071
SerhadDaimia <59991690+SerhadDaimia@users.noreply.github.com>
2021-04-09 01:14:55 +0300

    Update Program.cs

--- gitflowdeneme/Program.cs
+++ gitflowdeneme/Program.cs
@@ -10,7 +10,7 @@ namespace gitflowdeneme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sa caklıt");
+            Console.WriteLine("sa caklıts");
             Console.WriteLine("bas2");
             Console.WriteLine("yanıyormaster");
         }
